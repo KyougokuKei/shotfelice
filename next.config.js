891a9d9350251b8c10bfb833d6917f3d8c9e5027
karutt <karutetto332@gmail.com>
2022-02-09 14:41:39 +0900

    Webpack Bundle Analyzerの追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
-  reactStrictMode: true,
+const withPlugins = require("next-compose-plugins");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const i18n = {
   i18n: {
-    locales: ["ja"],
+    locales: ["en", "ja"],
     defaultLocale: "ja",
   },
-}
+};
+
+module.exports = withPlugins([withBundleAnalyzer({}), i18n]);
