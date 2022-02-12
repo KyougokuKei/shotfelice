309521d65ca906672ec80cf286bb60ea917b2612
karutt <karutetto332@gmail.com>
2022-02-12 14:19:14 +0900

    Buttonコンポーネントの追加

---
+++ components/Button.js
@@ -0,0 +1,32 @@
+
+import { Clickable, Text } from "../styles/components";
+import Link from 'next/link';
+export function Button(props) {
+
+    return (
+        <Link href={props.href} passHref>
+            <Clickable
+                height={50}
+                styles={{ boxSizeing: "border-box", cursor: "pointer" }}
+                className="border-box pointer"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                backgroundColor="#4d4646"
+                width={props.width}
+                {...props}
+            >
+                <Text
+                    px={props.px === undefined ? 40 : props.px}
+                    height="auto"
+                    fontSize={props.fontSize === undefined ? 18 : props.fontSize}
+                    color="white"
+                    display="flex" alignItems="center" justifyContent="center"
+                    height={50}
+                >
+                    {props.children}
+                </Text>
+            </Clickable>
+        </Link>
+    );
+}
