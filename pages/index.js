9fc88f1a055d90db7784329adcab638e28eb01aa
karutt <karutetto332@gmail.com>
2022-02-22 20:56:47 +0900

    撮影サービスのヘッダーを追加

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import { Button } from '../components/Button';
 import OutsideClickHandler from 'react-outside-click-handler';
 
 import { SlickSlider } from '../components/SlickSlider/Slider';
-import { Logo, Instagram, Phone, Send, Man, Car, Edit } from "../public/img/svg";
+import { Logo, Instagram, Phone, Send } from "../public/img/svg";
 
 import { convertGoldText } from "../lib/convert";
 
@@ -68,62 +68,88 @@ export default function Home({ data, imgPaths }) {
 
 function Service({ service }) {
   return (
-    Object.keys(service).map((key, index) => {
-      const isOdd = index % 2 === 0;
-      const isFinal = index === Object.keys(service).length - 1;
-
-      return (
-        <Box
-          key={index}
-          width="100vw"
-          height={["auto", 480, 580]}
-          background={isOdd ? "#F8F9FA" : "#FFFFFF"}
-          display="flex" alignItems="center" justifyContent="space-between"
-          flexDirection={["column", isOdd ? "row-reverse" : "row"]}
-          pb={[40, 0]}
+    <Box>
+      <Box
+        background="#F8F9FA"
+        pb={[0, 40]}
+        pt={[60, 80]}
+        display="flex" alignItems="center" justifyContent="center" flexDirection="column"
+      >
+        <Text
+          pl={[0, 6]}
+          pb={10}
+          fontSize={16}
+          color="grey5"
         >
-          <Box width={["100%", "50%", "50%"]} height="100%" display="flex" alignItems="flex-end" justifyContent="center">
-            <Box position="relative" width="100%" height="auto" mb={[40, 0]} maxWidth={["none", 480, 580]}>
-              <Image
-                src={"/img/service/" + key + ".png"}
-                layout="responsive" width={500} height={500}
-                alt={key}
-                objectFit={"cover"}
-              />
-            </Box>
-          </Box>
+          {service.title_en}
+        </Text>
+        <Text
+          fontSize={[30, 42, 42]}
+          fontWeight="bold"
+          color="black"
+          mb={20}
+        >
+          {service.title_jp}
+        </Text>
+      </Box>
 
+      {Object.keys(service.feature).map((key, index) => {
+        const isOdd = index % 2 === 0;
+        const isFinal = index === Object.keys(service.feature).length - 1;
+
+        return (
           <Box
-            display="flex"
-            flexDirection="column"
-            fontWeight="bold"
-            width={["100%", "50%", "50%"]}
-            px={"5%"}
+            key={index}
+            width="100vw"
+            height={["auto", 480, 580]}
+            background={isOdd ? "#F8F9FA" : "#FFFFFF"}
+            display="flex" alignItems="center" justifyContent="space-between"
+            flexDirection={["column", isOdd ? "row-reverse" : "row"]}
+            pb={[40, 0]}
           >
-            <Box
-              fontSize={[32, 32, 46]}
-              lineHeight={1.4}
-              mb={[20, 32]}
-              textAlign={isOdd ? "left" : "right"}
-            >
-              {convertGoldText(service[key].title)}
-            </Box>
-            <Box
-              color='grey5'
-              lineHeight={1.8}
-              textAlign={isOdd ? "left" : "right"}
-            >
-              {service[key].sub_title}
-            </Box>
-            {isFinal && (
-              <Box display="flex" alignItems="center" justifyContent="flex-end">
-                <Button mt={[32]} href="/reservation" width={["100%", "100%", 400]} fontWeight="normal" py={30} >撮影を予約</Button>
+            <Box width={["100%", "50%", "50%"]} height="100%" display="flex" alignItems="flex-end" justifyContent="center">
+              <Box position="relative" width="100%" height="auto" mb={[40, 0]} maxWidth={["none", 480, 580]}>
+                <Image
+                  src={"/img/service/" + key + ".png"}
+                  layout="responsive" width={500} height={500}
+                  alt={key}
+                  objectFit={"cover"}
+                />
               </Box>
-            )}
+            </Box>
+
+            <Box
+              display="flex"
+              flexDirection="column"
+              fontWeight="bold"
+              width={["100%", "50%", "50%"]}
+              px={"5%"}
+            >
+              <Box
+                fontSize={[32, 32, 46]}
+                lineHeight={1.4}
+                mb={[20, 32]}
+                textAlign={isOdd ? "left" : "right"}
+              >
+                {convertGoldText(service.feature[key].title)}
+              </Box>
+              <Box
+                color='grey5'
+                lineHeight={1.8}
+                textAlign={isOdd ? "left" : "right"}
+              >
+                {service.feature[key].sub_title}
+              </Box>
+              {isFinal && (
+                <Box display="flex" alignItems="center" justifyContent="flex-end">
+                  <Button mt={[32]} href="/reservation" width={["100%", "100%", 400]} fontWeight="normal" py={30} >撮影を予約</Button>
+                </Box>
+              )}
+            </Box>
           </Box>
-        </Box>
-      )
-    })
+        )
+      })}
+    </Box>
   )
 }
 
